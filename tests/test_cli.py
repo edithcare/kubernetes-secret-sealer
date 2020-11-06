0dfb35d12c5c57eb7efb606ef150d7c4e8342395
Daniel Tinney <dnltinney@gmail.com>
2020-11-06 11:12:33 +0100

    testint only first line

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,22 +8,22 @@ def runner():
     return CliRunner()
 
 
-def test_cli(runner):
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert not result.exception
-    assert result.output.strip() == 'Hello, world.'
+# def test_cli(runner):
+#     result = runner.invoke(cli.main)
+#     assert result.exit_code == 0
+#     assert not result.exception
+#     assert result.output.strip() == 'Hello, world.'
 
 
 def test_cli_with_option(runner):
-    result = runner.invoke(cli.main, ['--as-cowboy'])
+    result = runner.invoke(cli.main, ['--help'])
     assert not result.exception
     assert result.exit_code == 0
-    assert result.output.strip() == 'Howdy, world.'
+    assert result.output.split("\n")[0] == 'Usage: main [OPTIONS]'
 
 
-def test_cli_with_arg(runner):
-    result = runner.invoke(cli.main, ['Michael'])
-    assert result.exit_code == 0
-    assert not result.exception
-    assert result.output.strip() == 'Hello, Michael.'
+# def test_cli_with_arg(runner):
+#     result = runner.invoke(cli.main, ['Michael'])
+#     assert result.exit_code == 0
+#     assert not result.exception
+#     assert result.output.strip() == 'Hello, Michael.'
